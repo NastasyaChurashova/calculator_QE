46094c0f5bf9879fbb1c84e80d0e3c220e9e6762
Nastasya.Churashova <gregarious@list.ru>
2021-02-15 22:23:49 +0200

    finalized condition if

--- calculator4.php
+++ calculator4.php
@@ -71,18 +71,18 @@ if (array_key_exists('number-1', $_GET) &&
     isset($_GET['number-2']) && 
     isset($_GET['number-3']))  {
         
-        if ($Discriminant === 0){           
+        if ($Discriminant === 0 && $a != 0){           
             echo 'x=' . -$b / (2 * $a);
             }
    
-        elseif ($Discriminant > 0) {
+        elseif ($Discriminant > 0 && $a != 0) {
             $x1 = (-$b + sqrt($b**2 - 4*$a*$c)) / (2*$a);
             $x2 = (-$b - sqrt($b**2 - 4*$a*$c)) / (2*$a);
             echo  "x1 = $x1  "; 
             echo  "x2 = $x2";
             }
 
-        elseif ($Discriminant < 0) {
+        elseif ($Discriminant < 0 && $a != 0) {
             echo "no roots" ;
             
         }
